b6991015bb914c48769c95c4540c2dd56d0fb9f6
Jeroen Van den Berghe <yvandenberghe.jeroen@gmail.com>
2014-01-18 17:34:40 +0100

    Trying out some controller stuff

--- js/angular.controllers.js
+++ js/angular.controllers.js
@@ -2,6 +2,11 @@ var liveArticle = angular.module('liveArticle', []);
 
 liveArticle.controller('liveArticleCtrl', function ($scope, $http) {
 	$http.get('data/article130.json').success(function(data) {
+
+		console.log(data.title);
+
 		$scope.article = data;
+		
+		$scope.jeroen = data.title;
 	});
 });
